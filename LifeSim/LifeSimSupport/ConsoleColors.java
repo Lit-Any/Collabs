64be45a08e6224377ce4e230d889ea2ce137b06e
Lit-Any <siesta_sapient_6l@icloud.com>
2025-08-26 03:06:47 +0530

    Added methods to change text colour

---
+++ LifeSim/LifeSimSupport/ConsoleColors.java
@@ -0,0 +1,9 @@
+package LifeSim.LifeSimSupport;
+
+public class ConsoleColors {
+    public static final String reset = "\u001B[0m";
+    public static final String red = "\u001B[31m";
+    public static final String green = "\u001B[32m";
+    public static final String yellow = "\u001B[33m";
+    // Add more colors as needed
+}
